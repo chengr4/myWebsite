5befd050d2af73bbba336821c5ec1544e741bef0
Eddie Cheng <54136390+fscheng4747@users.noreply.github.com>
2020-11-14 15:15:44 +0100

    Updates

--- script_api.js
+++ script_api.js
@@ -1,5 +1,5 @@
 // Medium’s API is a JSON-based OAuth2 API. All requests are made to endpoints beginning:
-const medium_API_url = "https://api.medium.com/v1";
+/*const medium_API_url = "https://api.medium.com/v1";
 
 fetch(medium_API_url)
   .then(function(response) {
@@ -7,4 +7,15 @@ fetch(medium_API_url)
   })
   .then(function(myJson) {
     console.log(myJson);
-  });
+  });*/
+
+  const fn = document.querySelector("#first-note");
+
+  fn.addEventListener("click", test);
+
+  function test() {
+    console.log('link click');
+
+    //redirect 
+    location.href = 'https://fscheng4747.medium.com/algorithm-nodes-b5166a825c68';
+  }
