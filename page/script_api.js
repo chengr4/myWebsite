dbe9c434e28fa36693a17ca2a93e1e52ad8de107
Eddie Cheng <54136390+fscheng4747@users.noreply.github.com>
2020-11-22 07:05:12 +0800

    finish ES6 template literal

--- page/script_api.js
+++ page/script_api.js
@@ -9,13 +9,46 @@ fetch(medium_API_url)
     console.log(myJson);
   });*/
 
-  const fn = document.querySelector("#first-note");
+import {  article_left, article_center,  article_right } from './alter.js';
 
-  fn.addEventListener("click", test);
+// 文章欄 template
+const article_template = 
+  `
+  <div class="post">
+    <div class="post-preview">
+    <h6 class="post-title">${article_left.title}</h6>
+    <p class="post-intro">${article_left.introduction}</p>
+    <a  href=${article_left.url}>Read More</a>
+    </div>
+  </div>
 
-  function test() {
-    console.log('link click');
+  <div class="post">
+    <div class="post-preview">
+    <h6 class="post-title">${article_center.title}</h6>
+    <p class="post-intro">${article_center.introduction}</p>
+    <a  href=${article_center.url}>Read More</a>
+    </div>
+  </div>
 
-    //redirect 
-    location.href = 'https://fscheng4747.medium.com/algorithm-nodes-b5166a825c68';
-  }
+  <div class="post">
+    <div class="post-preview">
+    <h6 class="post-title">${article_right.title}</h6>
+    <p class="post-intro">${article_right.introduction}</p>
+    <a  href=${article_right.url}>Read More</a>
+    </div>
+  </div> `;
+
+document.querySelector('#posts').innerHTML = article_template;
+
+/* 沒在用，保留 code 日後可能有用
+const fn = document.querySelector("#first-note");
+
+fn.addEventListener("click", test);
+
+function test() {
+  console.log('link click');
+
+  //redirect 
+  location.href = 'https://fscheng4747.medium.com/algorithm-nodes-b5166a825c68';
+}
+*/
