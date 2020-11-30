3c18d77c792f1a849b81b13aa7166c5cc94c3e6d
Eddie Cheng <54136390+fscheng4747@users.noreply.github.com>
2020-11-30 14:11:37 +0100

    Updates

---
+++ alter.js
@@ -0,0 +1,18 @@
+// 替換文章 (左中右)
+export const article_left = {
+  url: "https://fscheng4747.medium.com/algorithm-notes-graph-1798ae2e5bcb",
+  title:'演算法筆記-Graph',
+  introduction:'Graph 表示物件之間的關係，並且最重要的是 Edge'
+};
+
+export const article_center = {
+  url: "https://fscheng4747.medium.com/%E8%A8%88%E7%AE%97%E6%A9%9F%E7%B6%B2%E8%B7%AF%E5%92%8C%E7%B6%B2%E9%9A%9B%E7%B6%B2%E8%B7%AF-network-layer-1713307c7e1b",
+  title:'計算機網路和網際網路 - Network layer',
+  introduction:'Computer Networks and Internet - Network layer'
+};
+
+export const article_right = {
+  url: "https://fscheng4747.medium.com/%E4%BB%80%E9%BA%BC%E6%98%AF-compile-to-js-32e4a1857daa",
+  title:'什麼是 Compile-to-JS?',
+  introduction:'It’s better to describe the process “compile” as “transpilation.” — Tom from Stack Overflow'
+};
